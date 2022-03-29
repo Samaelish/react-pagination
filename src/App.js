3f578b57cd9e35b9578fac27a7d50f288d322909
Samaelish <semnd007@gmail.com>
2022-03-29 16:51:38 +0300

    finished version with bugfix for 1st and last page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import Countries from './components/Countries'
+import Pagination from './components/Pagination'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [countriesPerPage] = useState(10)
+
+    useEffect(() => {
+        const getCountries = async () => {
+            setLoading(true)
+            const res = await axios.get('https://restcountries.com/v3.1/all')
+            const data = await res.data
+            setCountries(data)
+            setLoading(false)
+        }
+
+        getCountries()
+    }, [])
+
+    const lastCountryIndex = currentPage * countriesPerPage
+    const firstCountryIndex = lastCountryIndex - countriesPerPage
+    const currentCountries = countries.slice(firstCountryIndex, lastCountryIndex)
+
+    const paginate = pageNumber => setCurrentPage(pageNumber)
+
+    const nextPage = () => {
+        if(currentPage !== 25) {
+            setCurrentPage(prev => prev + 1)
+        }
+    }
+    const prevPage = () => {
+        if(currentPage !== 1) {
+            setCurrentPage(prev => prev - 1)
+        } 
+    }
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-primary">Countries</h1>
+            <Countries countries={currentCountries} loading={loading} />
+            <Pagination paginate={paginate} countriesPerPage={countriesPerPage} totalCountries={countries.length}/>
+            <button className="btn btn-primary" onClick={prevPage}>Prev Page</button>
+            <button className="btn btn-primary ms-2" onClick={nextPage}>Next Page</button>
+        </div>
+    )
 }
 
 export default App;
